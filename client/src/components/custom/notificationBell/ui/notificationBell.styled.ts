aa99afc208e4eb040bcd0c29ecaaed4ced36db25
Doonu <DonuSuzerain@yandex.ru>
2024-03-20 22:06:39 +0300

    ref: исправлены нотификации

--- client/src/components/custom/notificationBell/ui/notificationBell.styled.ts
+++ client/src/components/custom/notificationBell/ui/notificationBell.styled.ts
@@ -2,10 +2,21 @@ import styled from 'styled-components';
 
 export const SNotificationBellStyled = styled.div`
   width: 400px;
-  background: #2b2d31;
-  border: 1px solid #4c4d52;
+  background: ${({ theme }) => theme.colors.secondaryBg};
+  border-top: 1px solid ${({ theme }) => theme.colors.secondaryText};
+  border-left: 1px solid ${({ theme }) => theme.colors.secondaryText};
+  border-right: 1px solid ${({ theme }) => theme.colors.secondaryText};
   margin-top: 28px;
   color: ${({ theme }) => theme.colors.active};
+`;
+
+export const SList = styled.div`
+  max-height: 450px;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const SContent = styled.div`
